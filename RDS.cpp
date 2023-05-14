2ae62cd9604faf8007c4e7ccb257e3390718d2f2
Rogerkapa <101905494+Rogerkapa@users.noreply.github.com>
2023-05-14 12:26:27 +0100

    Add files via upload

---
+++ RDS.cpp
@@ -0,0 +1,468 @@
+/*
+  RDS ENCODER ARDUINO DUE modded from Jonas work
+*/
+
+#include "RDS.h"
+
+void RDSClass::begin(uint32_t period) {
+  SineTable_UP(2, 155);  //255    LEVEL OUTPUT
+
+  // SOFT INTERRUPT
+  NVIC_ClearPendingIRQ(SOFT_IRQn);
+  NVIC_SetPriority(SOFT_IRQn, 1);
+  NVIC_EnableIRQ(SOFT_IRQn);
+
+  // Configure DAC
+  pmc_enable_periph_clk (DACC_INTERFACE_ID) ; // start clocking DAC
+  dacc_reset(DACC);
+  dacc_set_transfer_mode(DACC, 0);
+  dacc_set_power_save(DACC, 0, 1);      // sleep = 0, fastwkup = 1
+  dacc_set_analog_control(DACC, DACC_ACR_IBCTLCH0(0x02) | DACC_ACR_IBCTLCH1(0x02) | DACC_ACR_IBCTLDACCORE(0x01));
+  dacc_set_trigger(DACC, 1);
+  dacc_set_channel_selection(DACC, 1);
+  dacc_enable_channel(DACC, 1);
+  //dacc_set_channel_selection(DACC, 0);
+  //dacc_enable_channel(DACC, 0);
+
+  NVIC_DisableIRQ(DACC_IRQn);
+  NVIC_ClearPendingIRQ(DACC_IRQn);
+  NVIC_SetPriority(DACC_IRQn, 0);
+  NVIC_EnableIRQ(DACC_IRQn);
+  dacc_enable_interrupt(DACC, DACC_IER_ENDTX);
+
+  DACC->DACC_TPR  =  (uint32_t)SignalTable_57khz;      // DMA buffer
+  DACC->DACC_TCR  =  480;  //480;
+  DACC->DACC_TNPR =  (uint32_t)SignalTable_57khz;      // next DMA buffer
+  DACC->DACC_TNCR =  30;   //30;
+  DACC->DACC_PTCR =  0x00000100;  //TXTEN - 8, RXTEN - 1.
+
+  // Configure Timer Counter to trigger DAC
+  // --------------------------------------
+  pmc_enable_periph_clk(TC_INTERFACE_ID + 0 * 3 + 0);
+
+  t->TC_CCR = TC_CCR_CLKDIS;
+  t->TC_IDR = 0xFFFFFFFF;
+  t->TC_SR;
+  t->TC_CMR = TC_CMR_TCCLKS_TIMER_CLOCK1 |
+              TC_CMR_WAVE |
+              TC_CMR_WAVSEL_UP_RC |
+              TC_CMR_EEVT_XC0 |
+              TC_CMR_ACPA_CLEAR | TC_CMR_ACPC_CLEAR |
+              TC_CMR_BCPB_CLEAR | TC_CMR_BCPC_CLEAR;
+
+  t->TC_RC = TCdivider_2;
+  t->TC_RA = TCdivider_2 / 2;
+  t->TC_CMR = (t->TC_CMR & 0xFFF0FFFF) | TC_CMR_ACPA_CLEAR | TC_CMR_ACPC_SET;
+  t->TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG;
+
+  // Configure pins
+  PIO_Configure(g_APinDescription[DAC0].pPort,
+                g_APinDescription[DAC0].ulPinType,
+                g_APinDescription[DAC0].ulPin,
+                g_APinDescription[DAC0].ulPinConfiguration);
+  PIO_Configure(g_APinDescription[DAC1].pPort,
+                g_APinDescription[DAC1].ulPinType,
+                g_APinDescription[DAC1].ulPin,
+                g_APinDescription[DAC1].ulPinConfiguration);
+
+  // Enable interrupt controller for DAC
+  dacc_disable_interrupt(DACC, 0xFFFFFFFF);
+  NVIC_DisableIRQ(DACC_IRQn);
+  NVIC_ClearPendingIRQ(DACC_IRQn);
+  NVIC_SetPriority(DACC_IRQn, 0);
+  NVIC_EnableIRQ(DACC_IRQn);
+  dacc_enable_interrupt(DACC, DACC_IER_ENDTX);
+}
+
+void RDSClass::end() {
+  TC_Stop(TC0, 1);
+  NVIC_DisableIRQ(DACC_IRQn);
+}
+
+bool RDSClass::PILOT_sync_state(void) const {
+  return sync_OK;
+}
+
+void RDSClass::PS_Set(char* pps) {
+    for (int i = 0; i < 8; i++) PS_NAME[i] = *(pps + i);
+}
+
+/*---SCROLL PS---*/
+void RDSClass::scroll_ps(bool enable) {
+  static int point_pos = 0, cicle_count = 0;
+
+  if (enable) {
+    if (cicle_count > 1) {
+      cicle_count = 0;
+      if (*PS_Pointer) {
+        PS_Pointer = DINAMIC_PS + point_pos++;
+      }
+      else {
+        PS_Pointer = DINAMIC_PS;
+        point_pos = 0;
+      }
+    }
+    cicle_count++;
+  }
+  else {
+    PS_Pointer = PS_NAME;
+    point_pos = 0;
+  }
+}
+
+
+void RDSClass::SendingGroups(void) {
+  static int group_i = 0;
+  static bool d_ps = 0;
+
+
+  if (!new_group_end) {
+    if (1) {
+
+      if (DINAMIC_PS_ON_OFF && d_ps) sendGroup(Group_0A);
+      else {
+        sendGroup(GROUPS[group_i]);
+        d_ps = !d_ps;
+        group_i++;
+      }
+      new_group_end = true;
+
+      if (group_i > 4) group_i = 0;
+    }
+    else group_i = 0;
+  }
+}
+/*---SENDING LOOP END---
+  =======================================================================================================================================
+  ---SEND GROUP---*/
+int RDSClass::sendGroup(Group_type group) {
+
+  setBlock(PI_CODE, Block_A);
+
+  switch (group)
+  {
+    case Group_0A:
+      setBlock((((word)group) << 11) | (((word)TP) << 10) | (((word)PTY) << 5) | (((word)TA) << 4) | (((word)M_S) << 3) | (((word)DI[di_ps_i / 2]) << 2) | (word)(di_ps_i / 2), Block_B);
+      setBlock(ALT_FREQ, Block_C);
+      setBlock((((word) * (PS_Pointer + di_ps_i++)) << 8) | *(PS_Pointer + di_ps_i++), Block_D);
+      if (di_ps_i > 7) {
+        di_ps_i = 0;
+        scroll_ps(DINAMIC_PS_ON_OFF);
+      }
+      break;
+
+    case Group_0B:
+      setBlock((((word)group) << 11) | (((word)TP) << 10) | (PTY << 5) | (((word)TA) << 4) | (((word)M_S) << 3) | (((word)DI[di_ps_i / 2]) << 2) | (di_ps_i / 2), Block_B);
+      setBlock(PI_CODE, Block_C_prime);
+      setBlock((((word) * (PS_Pointer + di_ps_i++)) << 8) | *(PS_Pointer + di_ps_i++), Block_D);
+      if (di_ps_i > 7) {
+        di_ps_i = 0;
+        scroll_ps(DINAMIC_PS_ON_OFF);
+      }
+      break;
+
+    case Group_1A:
+
+      break;
+
+    case Group_1B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_2A:
+      setBlock((((word)group) << 11) | (((word)TP) << 10) | ((word)PTY << 5) | (((word)RT_A_B_FLAG) << 4) | (rt_i / 4), Block_B);
+      setBlock((((word)RT_MESSAGE[rt_i++]) << 8) | RT_MESSAGE[rt_i++], Block_C);
+      setBlock((((word)RT_MESSAGE[rt_i++]) << 8) | RT_MESSAGE[rt_i++], Block_D);
+      if (rt_i > 63) rt_i = 0;
+      break;
+
+    case Group_2B:
+      if (rt_i > 31) rt_i = 0;
+      setBlock((((word)group) << 11) | (((word)TP) << 10) | ((word)PTY << 5) | (((word)RT_A_B_FLAG) << 4) | (rt_i / 4), Block_B);
+      setBlock(PI_CODE, Block_C_prime);
+      setBlock((((word)RT_MESSAGE[rt_i++]) << 8) | RT_MESSAGE[rt_i++], Block_D);
+      if (rt_i > 31) rt_i = 0;
+      break;
+
+    case Group_3A:
+      setBlock((((word)group) << 11) | (((word)TP) << 10) | ((word)PTY << 5) | ((word)Group_11A), Block_B);
+      setBlock((rtPlus_rfu << 13) | (((word)rtPlus_CB_flag) << 12) | (rtPlus_SCB_flag << 8), Block_C);
+      setBlock(0100101111010111, Block_D);
+      break;
+
+    case Group_3B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_4A:
+
+      break;
+
+    case Group_4B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_5A:
+
+      break;
+
+    case Group_5B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_6A:
+
+      break;
+
+    case Group_6B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_7A:
+
+      break;
+
+    case Group_7B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_8A:
+
+      break;
+
+    case Group_8B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_9A:
+
+      break;
+
+    case Group_9B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_10A:
+      setBlock((((word)group) << 11) | (((word)TP) << 10) | (((word)PTY) << 5) | (((word)PTYN_FLAG) << 4) | (word)(ptyn_i / 4), Block_B);
+      setBlock((((word) * (PTYN_NAME + ptyn_i++)) << 8) | *(PTYN_NAME + ptyn_i++), Block_C);
+      setBlock((((word) * (PTYN_NAME + ptyn_i++)) << 8) | *(PTYN_NAME + ptyn_i++), Block_D);
+      if (ptyn_i > 7) {
+        ptyn_i = 0;
+      }
+      break;
+
+    case Group_10B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_11A:
+
+      break;
+
+    case Group_11B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_12A:
+
+      break;
+
+    case Group_12B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_13A:
+
+      break;
+
+    case Group_13B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_14A:
+
+      break;
+
+    case Group_14B:
+      setBlock(PI_CODE, Block_C_prime);
+      break;
+
+    case Group_15A:
+      setBlock((((word)group) << 11) | (((word)TP) << 10) | (((word)PTY) << 5) | (((word)TA) << 4) | (word)(di_ps_i / 4), Block_B);
+      setBlock((((word) * (PS_Pointer + di_ps_i++)) << 8) | *(PS_Pointer + di_ps_i++), Block_C);
+      setBlock((((word) * (PS_Pointer + di_ps_i++)) << 8) | *(PS_Pointer + di_ps_i++), Block_D);
+      if (di_ps_i > 7) {
+        di_ps_i = 0;
+      }
+
+      break;
+
+    case Group_15B:
+      setBlock((((word)group) << 11) | (((word)TP) << 10) | ((word)PTY << 5) | (((word)RT_A_B_FLAG) << 4) | (rt_i / 4), Block_B);
+      setBlock(PI_CODE, Block_C_prime);
+      setBlock((((word)group) << 11) | (((word)TP) << 10) | ((word)PTY << 5) | (((word)RT_A_B_FLAG) << 4) | (rt_i / 4), Block_D);
+      if (di_ps_i > 7) {
+        di_ps_i = 0;
+      }
+      break;
+  }
+}
+
+void RDSClass::setBlock(word message, offset_type offset)
+{
+  int bite = 0;
+  static int bytecount = 0;
+  static int BytePointer = 0;
+  switch (offset)
+  {
+    case Block_A:
+      BytePointer = 0;
+      break;
+    case Block_B:
+      BytePointer = 1;
+      break;
+    case Block_C:
+      BytePointer = 2;
+      break;
+    case Block_C_prime:
+      BytePointer = 2;
+      break;
+    case Block_D:
+      BytePointer = 3;
+      break;
+  }
+  RDS_Data_Block_word[MEM_select][BytePointer] = (message << 10) | (crc(message) ^ (word)offset);
+}
+
+/*  ---CRC---*/
+word RDSClass::crc(word message)
+{
+  word crc = 0;
+  int message_bit;
+  int crc_bit;
+  for (int i = 0; i < 16; i++) //we are finished when all bits of the message are looked at
+  {
+    message_bit = (message >> 15) & 1;//bit of message we are working on. 15=block length-1
+    crc_bit = (crc >> 9) & 1;//bit of crc we are working on. 9=poly order-1
+
+    crc <<= 1;//shift to next crc bit (have to do this here before Gate A does its thing)
+    message <<= 1;//shift to next message bit
+
+    if (crc_bit ^ message_bit)crc = crc ^ 0x1B9; //add to the crc the poly mod2 if crc_bit + block_bit = 1 mod2
+
+  }
+  return (crc & 0x03FF); //ditch things above 10bits
+}
+
+
+/*---SIGNAL---*/
+void RDSClass::SineTable_UP(int new_rds_phase, int new_rds_level) {
+  const double pi = 3.141592653589793238462643383279502884197169399375105820974944;
+  int iii = NORMAL_PHASE_0;
+  int sample_57k = 10;  //10
+  static int old_rds_phase = 0; //0
+  static int old_rds_level = 0;
+
+  if ((old_rds_phase != new_rds_phase) && (old_rds_level != new_rds_level)) {
+    for (int i = 0; i < 72; i++) {    //72
+      for (int ii = 0; ii < sample_57k; ii++) {
+        SignalTable_57khz[iii++] = 2047 + sin((pi / 24) * i) * (sin((pi / (sample_57k / 2)) * ii) * (8 * new_rds_level));
+      }
+    }
+
+    iii = NORMAL_PHASE_1;
+    for (int i = 0; i < 96; i++) {     //96
+      for (int ii = 0; ii < sample_57k; ii++) {
+        SignalTable_57khz[iii++] = 2047 + ((sin((pi / 48) * i) * 2.5) + sin((pi / 16) * i)) * (sin((pi / (sample_57k / 2)) * ii) * (3 * new_rds_level));
+      }
+    }
+  }
+  old_rds_phase = new_rds_phase;
+  old_rds_level = new_rds_level;
+  //Serial.println(new_rds_phase);
+}
+
+RDSClass RDS;
+
+/*---ISR---*/
+void SOFT_Handler(void) {
+  RDS.SendingGroups();
+}
+
+/*---ISR---*/
+void DACC_Handler(void) {
+  if ((dacc_get_interrupt_status(DACC) & DACC_ISR_ENDTX) == DACC_ISR_ENDTX) {
+    RDS.DACC_ISR_ISR();
+  }
+}
+
+void RDSClass::DACC_ISR_ISR(void)
+{
+  static bool DataBit = false, PhaseShistBit = false;
+  static int BlockNumber = 0, bitcounter = 0, sync_c_1 = 0, sync_c_2 = 0;
+  static word RDS_Data_Block_bit_mask = (1 << 25);
+
+  //---PILOT SYNC--->>>---
+  if (digitalReadDirect(A1)) {
+    t->TC_RC = 73;
+    sync_c_1--;
+  }
+  else {
+    t->TC_RC = 74;
+    sync_c_1++;
+  }
+  if (sync_c_2 & (1 << 10)) {     //10
+    if ((sync_c_1 > 360) && (sync_c_1 < 397))    //355 392 INT _  EXT19KHZ =375-377
+      sync_OK = true;
+     //Serial.println("T");
+      
+    else sync_OK = false;
+    //Serial.println(sync_c_1);
+    sync_c_1 = 0;
+    sync_c_2 = 0;   
+  }
+  else {
+    sync_c_2++;
+  }
+  //---<<<---PILOT SYINC---
+
+  bitcounter += 30;
+  if (bitcounter >= 480) {      //480
+    bitcounter = 0;
+
+    if (RDS_Data_Block_word[MEM_select][BlockNumber] & RDS_Data_Block_bit_mask) {
+      PhaseShistBit = !PhaseShistBit;
+      DataBit = true;                 //true
+    }
+    else DataBit = false;             //false
+    if (PhaseShistBit) {
+      if (DataBit) PHASE_VAR = NORMAL_PHASE_1;
+      else PHASE_VAR = INVERT_PHASE_0;
+    }
+    else {
+      if (DataBit) PHASE_VAR = INVERT_PHASE_1;
+      else PHASE_VAR = NORMAL_PHASE_0;
+    }
+
+
+    //---------------------
+    RDS_Data_Block_bit_mask >>= 1;
+    if (!RDS_Data_Block_bit_mask) {
+      RDS_Data_Block_bit_mask = (1 << 25);
+
+      BlockNumber++;
+      if (BlockNumber > 3) {
+
+        NVIC_SetPendingIRQ(SOFT_IRQn);
+
+        BlockNumber = 0;
+        if (new_group_end) {
+          new_group_end = false;
+          MEM_select = !MEM_select;
+        }
+      }
+    }
+  }
+  DACC->DACC_TNPR = (uint32_t)SignalTable_57khz + (PHASE_VAR + bitcounter) * 2;
+  DACC->DACC_TNCR = 30;
+}
---
+++ RDS.h
@@ -0,0 +1,226 @@
+/*
+  RDS ENCODER ARDUINO DUE modded from Jonas work
+*/
+
+#ifndef RDS_INCLUDED
+#define RDS_INCLUDED
+
+#include <Arduino.h>
+#include "pins_arduino.h"  
+
+/******* SOFT INTERRUPT ---> *******/
+#define SOFT_IRQn TC1_IRQn
+#define SOFT_Handler TC1_Handler
+/******* <--- SOFT INTERRUPT *******/
+
+/***TIME CT***/
+#define RDS_GROUP_LENGTH 4
+#define RDS_BITS_PER_GROUP (RDS_GROUP_LENGTH * (RDS_BLOCK_SIZE+RDS_POLY_DEG))
+#define RDS_POLY 0x1B9
+#define RDS_POLY_DEG 10
+#define RDS_MSB_BIT 0x8000
+#define RDS_BLOCK_SIZE 16
+/***TIME CT***/
+
+
+class RDSClass
+{
+  public:
+
+    //    RDSClass();
+    void begin(uint32_t period);
+    void end();
+
+    typedef enum {Block_A = 0x0FC,
+                  Block_B = 0x198,
+                  Block_C = 0x168,
+                  Block_C_prime = 350,
+                  Block_D = 0x1B4,
+                  Block_E = 0
+                 } offset_type;
+
+
+    typedef enum {Group_0A, Group_0B,
+                  Group_1A, Group_1B,
+                  Group_2A, Group_2B,
+                  Group_3A, Group_3B,
+                  Group_4A, Group_4B,
+                  Group_5A, Group_5B,
+                  Group_6A, Group_6B,
+                  Group_7A, Group_7B,
+                  Group_8A, Group_8B,
+                  Group_9A, Group_9B,
+                  Group_10A, Group_10B,
+                  Group_11A, Group_11B,
+                  Group_12A, Group_12B,
+                  Group_13A, Group_13B,
+                  Group_14A, Group_14B,
+                  Group_15A, Group_15B
+                 } Group_type ;
+
+
+    typedef enum {PTY_No_programme_type_or_undefined,
+                  PTY_News,
+                  PTY_Current_affairs,
+                  PTY_Information,
+                  PTY_Sport,
+                  PTY_Education,
+                  PTY_Drama,
+                  PTY_Culture,
+                  PTY_Science,
+                  PTY_Varied,
+                  PTY_Pop_music,
+                  PTY_Rock_music,
+                  PTY_Easy_listening,
+                  PTY_Light_classical,
+                  PTY_Serious_classical,
+                  PTY_Other_music,
+                  PTY_Weather,
+                  PTY_Finance,
+                  PTY_Childrens_programmes,
+                  PTY_Social_affairs,
+                  PTY_Religion,
+                  PTY_Phone_in,
+                  PTY_Travel,
+                  PTY_Leisure,
+                  PTY_Jazz_music,
+                  PTY_Country_music,
+                  PTY_National_music,
+                  PTY_Oldies_music,
+                  PTY_Folk_music,
+                  PTY_Documentary,
+                  PTY_Alarm_test,
+                  PTY_Alarm
+                 } pty_type;
+
+    typedef enum {PTY_RBDS_No_program_type_or_undefined
+    , PTY_RBDS_News
+    , PTY_RBDS_Information
+    , PTY_RBDS_Sports
+    , PTY_RBDS_Talk
+    , PTY_RBDS_Rock
+    , PTY_RBDS_Classic_rock
+    , PTY_RBDS_Adult_hits
+    , PTY_RBDS_Soft_rock
+    , PTY_RBDS_Top_40
+    , PTY_RBDS_Country
+    , PTY_RBDS_Oldies
+    , PTY_RBDS_Soft
+    , PTY_RBDS_Nostalgia
+    , PTY_RBDS_Jazz
+    , PTY_RBDS_Classical
+    , PTY_RBDS_Rhythm_and_blues
+    , PTY_RBDS_Soft_rhythm_and_blues
+    , PTY_RBDS_Language
+    , PTY_RBDS_Religious_music
+    , PTY_RBDS_Religious_talk
+    , PTY_RBDS_Personality
+    , PTY_RBDS_Public
+    , PTY_RBDS_College
+    , PTY_RBDS_Spanish_Talk
+    , PTY_RBDS_Spanish_Music
+    , PTY_RBDS_Hip_Hop
+    , PTY_RBDS_Unassigned_1
+    , PTY_RBDS_Unassigned_2
+    , PTY_RBDS_Weather
+    , PTY_RBDS_Emergency_test
+    , PTY_RBDS_Emergency
+                 } pty_rbds_type;
+
+    void SineTable_UP(int new_rds_phase, int new_rds_level);
+
+    bool PS_mode(bool mode)  {
+      DINAMIC_PS_ON_OFF = mode;
+      return mode;
+    };
+
+    bool TP = false;
+    bool TA = false;
+    bool M_S = true;
+    /*---Decoder ID---*/
+    bool Stereo;
+    bool Artificial_Head;
+
+    void PS_Set(char* pps);
+
+    void PTYN_Set(char* pptyn) {
+      for (int i = 0; i < 8; i++) {
+        if (*pptyn != 0 ) PTYN_NAME[i] = *pptyn + i;
+        else PTYN_NAME[i] = (' ');
+      }
+      PTYN_FLAG = !PTYN_FLAG;
+    }
+
+    void RT_Set(char* prt) {
+      int i;
+      for (i = 0 ; (i < 64) && (*(prt + i) != '\0') ; i++) RT_MESSAGE[i] = *(prt + i);
+      for ( ; i < 64; i++) RT_MESSAGE[i] = (' ');
+      RT_A_B_FLAG = !RT_A_B_FLAG;
+    }
+
+    bool PILOT_sync_state(void) const;
+
+    void DACC_ISR_ISR(void);
+
+    Group_type GROUPS[10] = {Group_0A, Group_2A, Group_10A, Group_2A, Group_10A};
+
+    void SendingGroups(void);
+
+ 
+  private:
+
+    const int NORMAL_PHASE_0 = 0;
+    const int INVERT_PHASE_0 = 240;            
+    const int NORMAL_PHASE_1 = 720;            
+    const int INVERT_PHASE_1 = 1200;            
+
+    TcChannel * t = &(TC0->TC_CHANNEL)[0];
+
+    uint16_t SignalTable_57khz[1681];
+    uint32_t PHASE_VAR;
+
+    word PI_CODE = 0x9000;            //(BLOCK_A) , (Block_C_prime)
+
+    pty_type PTY = PTY_Pop_music;
+    char PTYN_NAME[9] = ("DANCE M");
+
+    /*---Decoder identification and Dynamic PTY indicator---*/
+    bool DI[4] {true, true, false, true};  //[0] Static PTY - Dinamic PTY, [1] Not compressed - Compressed,
+    //[2] Not Artificial Head - Artificial Head, [3] mono - stereo
+
+    word ALT_FREQ = (0xE0 << 8) | 0xE0;
+    char PS_NAME[9] = ("RDSTEST1");  
+    char DINAMIC_PS[65] = ("ARDUINO DUE RDS ENCODER INTERNAL DAC");
+    bool DINAMIC_PS_ON_OFF = false;
+    char RT_MESSAGE[65] = ("ARDUINO DUE RDS ENCODER INTERNAL DAC"); 
+   
+    bool RT_A_B_FLAG = false;                                            
+    bool PTYN_FLAG = true;
+    char* PS_Pointer = PS_NAME;
+    const int TCdivider_1 = 73;
+    const int TCdivider_2 = 74;
+    bool sync_OK;
+
+    int sendGroup(Group_type);
+    void scroll_ps(bool);
+    word crc(word);
+    void setBlock(word, offset_type);
+    word RDS_Data_Block_word[2][4];                                               //Ezzel a bittel váltunk az aktuálisan olvasott és az írandó tömbelem közt.
+    volatile bool new_group_end = false;
+    bool MEM_select = false;
+    int di_ps_i = 0, rt_i = 0, ptyn_i = 0;
+
+    word rtPlus_rfu = 0;
+    bool rtPlus_CB_flag = 0;
+    word rtPlus_SCB_flag = 0;
+    word rtPlus_Temp_num = 0;
+
+};
+
+inline int digitalReadDirect(int pin) {
+  return !!(g_APinDescription[pin].pPort -> PIO_PDSR & g_APinDescription[pin].ulPin);
+}
+
+extern RDSClass RDS;
+
+#endif
